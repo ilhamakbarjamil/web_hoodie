aa7ed7b72b4bc436a0270920fafd5ff3261a754e
ilhamakbarjamil <ilhamakbarjamil8@gmail.com>
2025-04-30 15:31:17 +0700

    first

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "hoodie_store";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
